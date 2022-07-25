90585d53b4b428af5f723a45ed151413e44bc404
Claudia Dias <claudiamld@outlook.com>
2022-07-25 10:22:00 -0300

    Render Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import ClickCounter from "./components/ClickCounter";
-import HoverCounter from "./components/HoverCounter";
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import Counter from "./components/Counter";
+import HoverCounterTwo from "./components/HoverCounterTwo";
+import User from "./components/User";
 
 function App() {
   return (
     <div>
-      <ClickCounter name="Claudia" />
-      <HoverCounter />
+      <Counter 
+        render={(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )} 
+      />
+      <Counter 
+        render={(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )} 
+      />
+      {/* <ClickCounterTwo />      
+      <HoverCounterTwo />
+      <User render={ (isLoggedIn) => isLoggedIn ? 'Claudia' : 'Guest'} /> */}
     </div>
   );
 }
